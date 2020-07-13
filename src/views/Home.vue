f850bd9532e7bf30a6f75cd722de3c1fd105a1e8
E6440\Kouts <koutsaftakis@gmail.com>
2020-07-13 12:51:59 +0300

    Removed generic vue-cli texts

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Vue.js starter template"/>
   </div>
 </template>
 
