50863f5b2b2f618b48d110b36940ba4ae8f3c83f
E6440\Kouts <koutsaftakis@gmail.com>
2020-08-07 00:48:29 +0300

    Removed unneeded config

--- vue.config.js
+++ vue.config.js
@@ -2,12 +2,6 @@ const path = require('path');
 
 module.exports = {
   lintOnSave: 'warning',
-  pluginOptions: {
-    'style-resources-loader': {
-      preProcessor: 'scss',
-      patterns: [path.resolve(__dirname, './src/scss/styles.scss')]
-    }
-  },
   css: {
     loaderOptions: {
       scss: {
